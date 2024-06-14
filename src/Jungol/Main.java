4c4d0d9c8a4499aa4f8e477c27a820b636e7e6cd
EdenKwon <ybinn@caresquare.kr>
2024-06-14 10:40:26 +0900

    #ADD - 출력 1~4번 추가

--- src/Jungol/Main.java
+++ src/Jungol/Main.java
@@ -5,6 +5,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //
+        test04();
+    }
+
+    public static void test01() {
+        System.out.println("Fun Programming!");
+    }
+
+    public static void test02() {
+        System.out.println("Programming! It's fun.");
+    }
+
+    public static void test03() {
+        System.out.println("My name is Hong Gil Dong.");
+        System.out.println("I am 13 years old.");
+    }
+
+    public static void test04() {
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
     }
 }
