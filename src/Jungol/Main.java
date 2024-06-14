50ceb54ca5616de54591917853e965073d9953dc
EdenKwon <ybinn@caresquare.kr>
2024-06-14 11:16:43 +0900

    #ADD - 출력 5~8번 추가

--- src/Jungol/Main.java
+++ src/Jungol/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        test04();
+        test08();
     }
 
     public static void test01() {
@@ -25,5 +25,29 @@ public class Main {
         System.out.println("(@) (@)");
         System.out.println("(=^.^=)");
         System.out.println("(-m-m-)");
+    }
+
+    public static void test05() {
+        System.out.println("I can program well.");
+        System.out.println("Dreams come true.");
+    }
+
+    public static void test06() {
+        System.out.println("My height\n" +
+                "170\n" +
+                "My weight\n" +
+                "68.600000");
+    }
+
+    public static void test07() {
+        System.out.println("5 Dan\n" +
+                "5 * 2 = 10");
+    }
+
+    public static void test08() {
+        System.out.println("      item     count     price\n" +
+                "       pen        20       100\n" +
+                "      note         5        95\n" +
+                "    eraser       110        97");
     }
 }
