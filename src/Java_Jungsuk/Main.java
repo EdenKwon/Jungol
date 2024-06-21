ddbc8cd3b20f4efbd8be015356a551390a305354
EdenKwon <ybinn@caresquare.kr>
2024-06-21 11:50:25 +0900

    #FIX - 10번 리팩토링 및 유효성 검사 메서드 추가

--- src/Java_Jungsuk/Main.java
+++ src/Java_Jungsuk/Main.java
@@ -1,9 +1,11 @@
 package Java_Jungsuk;
 
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-    ex04_3();
+    ex04_10();
   }
 
   public static boolean ex04_1(int x) {
@@ -146,10 +148,45 @@ public class Main {
     System.out.println("sum=" + sum);
   }
 
+  public static boolean isNumber(String input) {
+    return Pattern.matches("^[0-9]*$", input);
+  }
+
   public static void ex04_10() {
-    int answer = (int) (Math.random() * 100) + 1;
-    int input = 0;
-    int count = 0;
+    int answer = (int) (Math.random() * 100) + 1; //1~100까지의 랜덤한 수
+    String input = ""; // 사용자 입력 값
+    int count = 0; // 시도한 횟수
+
+    java.util.Scanner s = new java.util.Scanner(System.in);
+
+    do {
+      count++;
+      System.out.print("1과 100사이의 값을 입력하세요 :");
+      input = s.next();
+
+      if (!isNumber(input)) {
+        System.out.println("다시 입력해주세요. 형식이 맞지 않습니다.");
+      } else {
+        if (Integer.parseInt(input) > answer) {
+          System.out.println("더 작은 수를 입력하세요.");
+        } else if (Integer.parseInt(input) < answer) {
+          System.out.println("더 큰 수를 입력하세요.");
+        } else {
+          System.out.println("맞췄습니다.");
+          System.out.println("시도횟수는 " + count + "번입니다.");
+          break;
+        }
+      }
+    }
+    while (true);
+  }
+
+
+
+  public static void ex04_10_bu() {
+    int answer = (int) (Math.random() * 100) + 1; //1~100까지의 랜덤한 수
+    int input = 0; // 사용자 입력 값
+    int count = 0; // 시도한 횟수
 
     java.util.Scanner s = new java.util.Scanner(System.in);
 
@@ -158,15 +195,22 @@ public class Main {
       System.out.print("1과 100사이의 값을 입력하세요 :");
       input = s.nextInt();
 
-      if(input > answer) {
-        System.out.println("더 작은 수를 입력하세요.");
-      } else if(input < answer) {
-        System.out.println("더 큰 수를 입력하세요.");
+      if (!isNumber(String.valueOf(input))) {
+        System.out.println("다시 입력해주세요. 형식이 맞지 않습니다.");
       } else {
-        System.out.println("맞췄습니다.");
-        System.out.println("시도횟수는 " + count + "번입니다.");
-        break;
+        if (input > answer) {
+          System.out.println("더 작은 수를 입력하세요.");
+        } else if (input < answer) {
+          System.out.println("더 큰 수를 입력하세요.");
+        } else {
+          System.out.println("맞췄습니다.");
+          System.out.println("시도횟수는 " + count + "번입니다.");
+          break;
+        }
       }
-    } while (true);
+    }
+    while (true);
   }
 }
+
+
