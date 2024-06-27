dc173765f7bfba9f69046fd39a940e64934c8c09
EdenKwon <ybinn@caresquare.kr>
2024-06-27 18:29:44 +0900

    #FIX - 15번 for문 수정

--- src/ArrayTest/ArrayTest3/ArrayUtil.java
+++ src/ArrayTest/ArrayTest3/ArrayUtil.java
@@ -1,7 +1,8 @@
 package ArrayTest.ArrayTest3;
 
 public class ArrayUtil {
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     char[] s1 = {'J', 'a', 'v', 'a', 'P', 'r', 'o', 'g', 'r', 'a', 'm'};
     printString(reverseString(s1, 0, 3));
 
@@ -16,21 +17,21 @@ public class ArrayUtil {
   }
 
   public static char[] reverseString(char[] s, int start, int end) {
-    char temp[] = new char[end-start+1];
+    char temp[] = new char[end - start + 1];
 
-    for(int i = 0; i<end-start; i++) {
-      temp[start+i] = s[end-i];
+    for (int i = 0; i <= end - start; i++) {
+      temp[i] = s[end - i];
     }
 
-    for(int i= start; i<=end;i++) {
-      s[i] = temp[i];
+    for (int i = 0; i <= end - start; i++) {
+      s[start + i] = temp[i];
     }
 
     return s;
   }
 
   public static void printString(char[] s) {
-    for(char c : s) {
+    for (char c : s) {
       System.out.print(c);
     }
     System.out.println();
